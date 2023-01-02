3d20a5caae907a4e3645a9d1bd9c04a0605afb4e
Myat Thu <104615040+MyatGThu@users.noreply.github.com>
2023-01-03 10:40:25 +1100

    Local and Global scopes
    A variable is only available from inside the region it is created. This is called scope.
    
    A variable created inside a function belongs to the local scope of that function, and can only be used inside that function.
    
    A variable created in the main body of the Python code is a global variable and belongs to the global scope.
    
    If you need to create a global variable, but are stuck in the local scope, you can use the global keyword.
    
    The global keyword makes the variable global.

---
+++ scopes.py
@@ -0,0 +1,44 @@
+""" 
+Variables inside a function is only available inside that function, i.e. local scope.
+A variable is not available outside the function but is available for any function,
+inside the function.
+"""
+
+# Local scope fucntion e.g.
+
+def myfunc():
+  x = 30
+  def myinnterfunc(): #calling variable inside function.
+    print(x)
+  myinnerfunc()
+
+myfunc()
+
+# Global scope function e.g.
+
+x = 3000
+def myfunc():
+  x = 200
+  print(x)
+myfunc() #prints internal defined x = 200
+print(x) #prints global 3000.
+
+# Creating a global variable using 'global' keyword inside a local function.
+
+def myfunc():
+  global y
+  y = 3000
+
+myfunc()
+print(y)
+
+# Change the gloabl variable from a local function.
+
+x = 300
+
+def myfunc():
+  global x
+  x = 200
+myfunc() # prints out 200
+
+print(x) # changes the variable outside the function to 200.
