1a6ffe9211ba61f1a65892641a19d29a88b7653e
Myat Thu <104615040+MyatGThu@users.noreply.github.com>
2023-01-03 16:01:13 +1100

    closing objects and cleaning up resources

---
+++ try_except2.py
@@ -0,0 +1,13 @@
+# Using error handling to close objects and clean up resources
+
+try:
+  f = open("demofile.txt")
+  try:
+    f.write("Lorum Ipsum")
+  except:
+    print("Something went wrong when writing to the file")
+  finally:
+    f.close()
+except:
+  print("Something went wrong when opening the file")
+  
