82a15750adafce7d3e7651a40480804de37c1b76
Myat Thu <104615040+MyatGThu@users.noreply.github.com>
2023-01-03 11:35:58 +1100

    Using build in "math" module
    using the math module and some of its functions / constraints along with build in basic math functions such as min() / max().

---
+++ build_in_math.py
@@ -0,0 +1,39 @@
+# Improting math module
+import math
+
+# Build in min() / max() functions
+
+x = min(5,10,25)
+y = max(5,10,25)
+
+print(x)
+print(y)
+
+# abs() - absolute value of a number
+
+x = abs(-8.88)
+print(x)
+
+# pow(x,y) - power of y (x^y).
+
+x = pow(4,3)
+print(x)
+
+# using methods of "math" module
+x = math.sqrt(64)
+print(x)
+
+# math.ceil() - round decimal to nearest integer [upwards] math.floor() - round decimal [downwards]
+
+y = math.floor(2.4)
+x = math.ceil(2.4)
+
+print(x) # returns 3
+print(y) # returns 2
+
+# pi constant returns value of PI
+
+x = math.pi
+print(x)
+
+# https://www.w3schools.com/python/module_math.asp
