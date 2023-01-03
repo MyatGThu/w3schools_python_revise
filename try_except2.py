fad57ed2894c29fd8e93499a0f0c3d72e6d39549
Myat Thu <104615040+MyatGThu@users.noreply.github.com>
2023-01-03 16:08:05 +1100

    Update try_except2.py

--- try_except2.py
+++ try_except2.py
@@ -1,5 +1,4 @@
 # Using error handling to close objects and clean up resources
-
 try:
   f = open("demofile.txt")
   try:
@@ -11,3 +10,14 @@ try:
 except:
   print("Something went wrong when opening the file")
   
+# Raising exceptions
+# 'raise' keyword , Exception() method
+x = -1
+if x < 0:
+  raise Exception("Sorry, no numbers below zero")
+  
+# TypeError() method
+
+x = "batman"
+if not type(x) is int:
+  raise TypeError("Only intergers are allowed")
