b559add7956d2157c693e62c20ae29800e159204
Myat Thu <104615040+MyatGThu@users.noreply.github.com>
2023-01-06 08:31:29 +1100

    Matplotlib

---
+++ matplotlib1.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt # pip install matplotlib
+
+print(matplotlib.__version__) #checking version of installed matplotlib
+
+import numpy as np
+
+xpoints = np.array([0,6])
+ypoints = np.array([0,250])
+
+plt.plot(xpoints, ypoints)
+plt.show() #prints out restuls of the graph points
